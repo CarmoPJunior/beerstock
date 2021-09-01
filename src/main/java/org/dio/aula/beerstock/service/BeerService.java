f808221b039f984ce24ea2725f31cd4da3425288
carmoPjunior <carmo.goncalves.jr@hotmail.com>
2021-08-31 22:59:50 -0300

    Cria as exception e o controller

---
+++ src/main/java/org/dio/aula/beerstock/service/BeerService.java
@@ -0,0 +1,13 @@
+package org.dio.aula.beerstock.service;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class BeerService {
+
+
+}
