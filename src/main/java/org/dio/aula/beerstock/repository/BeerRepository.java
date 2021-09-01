e4e623eb7112ce9d0ccc776d864367ce1933907c
carmoPjunior <carmo.goncalves.jr@hotmail.com>
2021-08-31 22:54:16 -0300

    Cria o repository

---
+++ src/main/java/org/dio/aula/beerstock/repository/BeerRepository.java
@@ -0,0 +1,10 @@
+package org.dio.aula.beerstock.repository;
+
+import org.dio.aula.beerstock.entity.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name);
+}
